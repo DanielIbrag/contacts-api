027f4812d526cf8634c527c199c73ae810260a93
DanielIbrag <danny.ibragimov1@gmail.com>
2022-03-08 20:03:33 -0500

    pushing a way to add to db using seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Contact.create(first_name: "Daniel", last_name: "Ibrag", email: "Daniel@test.com", phone_number: "111-111-1111")
+Contact.create(first_name: "Jon", last_name: "Snow", email: "GoT@test.com", phone_number: "111-111-2222")
+Contact.create(first_name: "Chris", last_name: "Florence", email: "Chris@test.com", phone_number: "111-111-3333")
